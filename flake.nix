8744a045086466f5a5698c026d362a60195ca12b
bgl gwyng <bgl@gwyng.com>
2024-12-03 12:11:27 +0900

    Add `mergeTypes` option.

--- flake.nix
+++ flake.nix
@@ -1,27 +1,14 @@
 {
   outputs = _:
-    let
-      fold1 = op: list:
-        if list == [ ] then
-          abort "fold1: empty list"
-        else
-          builtins.foldl' op (builtins.head list)
-            (builtins.tail list);
-    in
+
     {
-      nixosModules.default = ({ lib, ... }:
+      nixosModules.default = ({ lib, config, ... }:
         let
           optionType = (lib.mkOptionType {
             name = "option-type";
             description = "option type";
             check = lib.types.isOptionType;
-            merge = loc: defs:
-              (fold1
-                (x: y:
-                  let merged = lib.defaultTypeMerge x.functor y.functor; in
-                  assert merged != null || abort "Cannot merge option types ${x.description} and ${y.description}";
-                  merged))
-                (map (def: def.value) defs);
+            merge = config.mergeTypes;
           });
         in
         {
@@ -30,6 +17,26 @@
             type = lib.types.attrsOf optionType;
           };
           config.t = lib.filterAttrs (_: lib.types.isOptionType) lib.types;
+
+          options.mergeTypes = lib.mkOption {
+            description = "merge types";
+            type = lib.types.functionTo (lib.types.functionTo (lib.types.anything));
+            default = loc: defs:
+              let
+                fold1 = op: list:
+                  if list == [ ] then
+                    abort "fold1: empty list"
+                  else
+                    builtins.foldl' op (builtins.head list)
+                      (builtins.tail list);
+              in
+              (fold1
+                (x: y:
+                  let merged = lib.defaultTypeMerge x.functor y.functor; in
+                  assert merged != null || abort "Cannot merge option types ${x.description} and ${y.description}";
+                  merged))
+                (map (def: def.value) defs);
+          };
         });
     };
 }
